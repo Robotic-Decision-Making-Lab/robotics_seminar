42b46fe3952926be7fc819d98ec69054cb239af3
Evan Palmer <evanp922@gmail.com>
2024-09-28 15:25:52 -0700

    Updated Fall 2024 schedule

---
+++ src/speakers/Winter2025.jsx
@@ -0,0 +1,11 @@
+export const Winter2025Speakers = [
+  {
+    date: "Nov 1",
+    name: "Matthew Gombolay",
+    affiliation: "Georgia Institute of Technology",
+    title: "TBD",
+    location: "Rogers Hall 230",
+    time: "10:00am",
+    url: "https://core-robotics.gatech.edu/people/matthew-gombolay/",
+  },
+];
